06882bbc97a3b8c5036994ba1bd5d933f08a9bd9
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-02-28 17:49:23 -0500

    General strcture for fizzbuzz numeric counting assignment

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+
+
+function fizzBuzz(x) {
+  let numbers = [];
+  for (let i = 1; i <= x; i++) {
+    //  console.log("I love " + i);
+     //if if else
+     //  is it a multiple of 3?
+     numbers.push(i);
+  }
+  return numbers;
+}
+
+let answer = fizzBuzz(8);
+console.log(answer);
--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 //sonnet file
 let sonnets = require('./sonnets.js');
 
-
-
 function wordFrequency(text) {
   let words = text.split( ' ' );
-  let frequency = {
-
-  };
+  let frequency = {};
 
   words.forEach( function listOut(eachWord) {
     if (eachWord !== '') {
@@ -17,29 +13,13 @@ function wordFrequency(text) {
         //repeat to add
         frequency[eachWord] = frequency[eachWord] + 1;
       }
-
     }
-
   });
   return frequency;
 }
 
-console.log(wordFrequency("NaN NaN NaN Batma"));
-
-//practice
-// let testString = "This is a string of words. Some words appear more than once. a word."
-// let test = testString.split( ' ' );
-//
-// test.forEach( function listOut(eachTestWord) {
-//   console.log(eachTestWord);
-// });
-
-
-
-
-
-
+console.log(wordFrequency(sonnets));
 
 //tests
-// let testRunner = require('./word-frequency-tests.js');
-// testRunner(wordFrequency);
+let testRunner = require('./word-frequency-tests.js');
+testRunner(wordFrequency);
