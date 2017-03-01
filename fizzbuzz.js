5e0a1c4e8b1fa558dae3bd576f975999eabbc608
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-02-28 22:16:44 -0500

    Produces buzz upon multiple of 3, fizz upon multiples of 5, and fizzbuzz upon multiples of both

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,13 +3,21 @@
 function fizzBuzz(x) {
   let numbers = [];
   for (let i = 1; i <= x; i++) {
-    //  console.log("I love " + i);
-     //if if else
-     //  is it a multiple of 3?
-     numbers.push(i);
+    let result;
+    if (i % 3 === 0 && i % 5 === 0) {
+      result = "fizzbuzz";
+    } else if (i % 5 === 0) {
+      result = "buzz";
+    } else if (i % 3 === 0) {
+      result = "fizz";
+    } else {
+       result = i;
+     }
+
+     numbers.push(result);
   }
   return numbers;
 }
 
-let answer = fizzBuzz(8);
+let answer = fizzBuzz(20);
 console.log(answer);
